48ad17b7f1645f63c9de67d34b1355496e88c83d
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-23 16:10:08 +0000

    more styles

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ function App() {
       <div className='bg-white px-5 pt-5 sm:w-[40rem] md:bg-blue-500 lg:bg-pink-500 xl:bg-purple-600 rounded-2xl'>
         <div className='flex justify-between items-center mb-5'>
           <div className='flex gap-2'>
-            <p className='font-semibold text-[1.3rem] text-Verydarkblue'>Notifications</p>
-            <p className='text-[1rem] font-extrabold text-White bg-Blue rounded-md px-3 items-center flex h-7'>{newMessage}</p>
+            <p className='font-semibold text-[1.3rem] text-Verydarkblue cursor-pointer'>Notifications</p>
+            <p className='text-[1rem] font-extrabold text-White bg-Blue rounded-md px-3 items-center flex h-7 cursor-pointer'>{newMessage}</p>
           </div>
           <p className='text-Grayishblue hover:text-Blue'>Mark all as read</p>
         </div>
@@ -27,7 +27,7 @@ function App() {
         <div className='bg-Verylightgrayishblue p-3 rounded-lg flex mb-3'>
           <img className='h-[2.8rem]' src={mark} alt="Mark's image" />
           <div className='ml-3 text-[0.9rem]'>
-            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue'>Mark Webber</span> reacted to your recent post <span className='font-semibold text-Darkgrayishblue hover:text-Blue'>My first tournament today!<GoDotFill className='inline text-Red h-4 w-4'/></span></p>
+            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer'>Mark Webber</span> reacted to your recent post <span className='font-semibold text-Darkgrayishblue hover:text-Blue cursor-pointer'>My first tournament today!<GoDotFill className='inline text-Red h-4 w-4'/></span></p>
             <p className='text-Lightgrayishblue2'>1m ago</p>
           </div>
         </div>
@@ -35,7 +35,7 @@ function App() {
         <div className='bg-Verylightgrayishblue p-3 rounded-lg flex mb-3'>
           <img className='h-[2.8rem]' src={angela} alt="Angela's image" />
           <div className='ml-3 text-[0.9rem]'>
-            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue'>Angela Gray</span> followed you<GoDotFill className='inline text-Red h-4 w-4'/></p>
+            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer'>Angela Gray</span> followed you<GoDotFill className='inline text-Red h-4 w-4'/></p>
             <p className='text-Lightgrayishblue2'>5m ago</p>
           </div>
         </div>
@@ -43,7 +43,7 @@ function App() {
         <div className='bg-Verylightgrayishblue p-3 rounded-lg flex mb-3'>
           <img className='h-[2.8rem]' src={jacob} alt="Jacob's image" />
           <div className='ml-3 text-[0.9rem]'>
-            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue'>Jacob Thompson</span> has joined your group <span className='font-semibold text-Blue'>Chess Club</span><GoDotFill className='inline text-Red h-4 w-4'/></p>
+            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer'>Jacob Thompson</span> has joined your group <span className='font-semibold text-Blue cursor-pointer'>Chess Club</span><GoDotFill className='inline text-Red h-4 w-4'/></p>
             <p className='text-Lightgrayishblue2'>1 day ago</p>
           </div>
         </div>
@@ -51,7 +51,7 @@ function App() {
         <div className=' p-3 rounded-lg flex mb-3'>
           <img className='h-[2.8rem]' src={rizky} alt="Rizky's image" />
           <div className='ml-3 text-[0.9rem]'>
-            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue'>Rizky Hasanuddin</span> sent you a private message</p>
+            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer'>Rizky Hasanuddin</span> sent you a private message</p>
             <p className='text-Lightgrayishblue2'>5 days ago</p>
             <p className='text-Grayishblue border p-4 rounded-md mt-4 outline-none border-Lightgrayishblue2'>Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.</p>
           </div>
@@ -61,7 +61,7 @@ function App() {
           <div className='flex'>
             <img className='h-[2.8rem]' src={kimberly} alt="Kimberly's image" />
             <div className='ml-3 text-[0.9rem]'>
-              <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue'>Kimberly Smith</span> commented on your picture</p>
+              <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer'>Kimberly Smith</span> commented on your picture</p>
               <p className='text-Lightgrayishblue2'>1 week ago</p>
             </div>
           </div>
@@ -71,7 +71,7 @@ function App() {
         <div className=' p-3 rounded-lg flex mb-4 sm:mb-0'>
           <img className='h-[2.8rem]' src={nathan} alt="Nathan's image" />
           <div className='ml-3 text-[0.9rem]'>
-            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue'>Nathan Peterson</span> reacted to your recent post <span className='font-semibold text-Darkgrayishblue hover:text-Blue'>5 end-game strategies to increase your win rate</span></p>
+            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer'>Nathan Peterson</span> reacted to your recent post <span className='font-semibold text-Darkgrayishblue hover:text-Blue cursor-pointer'>5 end-game strategies to increase your win rate</span></p>
             <p className='text-Lightgrayishblue2'>2 weeks ago</p>
           </div>
         </div>
@@ -79,7 +79,7 @@ function App() {
         <div className=' p-3 rounded-lg flex mb-10'>
           <img className='h-[2.8rem]' src={anna} alt="Anna's image" />
           <div className='ml-3 text-[0.9rem]'>
-            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue'>Anna Kim</span> left the group <span className='font-semibold text-Blue'>Chess Club</span></p>
+            <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer'>Anna Kim</span> left the group <span className='font-semibold text-Blue cursor-pointer'>Chess Club</span></p>
             <p className='text-Lightgrayishblue2'>2 weeks ago</p>
           </div>
         </div>
