9c36bb6ed7451a0bc7c3cc8428e18163a24a4cbc
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-23 20:34:06 +0000

    made some corrections

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
           <img className='h-[2.8rem] cursor-pointer' src={mark} alt="Mark's image" />
           <div className='ml-3 text-[0.9rem]'>
             <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer transition ease-in duration-150'>Mark Webber</span> reacted to your recent post <span className='font-semibold text-Darkgrayishblue hover:text-Blue cursor-pointer transition ease-in duration-150'>My first tournament today!<GoDotFill className={`inline ${clear || show[0]?'text-White':'text-Red'}  h-4 w-4`}/></span></p>
-            <p className='text-Lightgrayishblue2'>1m ago</p>
+            <p className='text-Grayishblue'>1m ago</p>
           </div>
         </div>
 
@@ -46,7 +46,7 @@ function App() {
           <img className='h-[2.8rem] cursor-pointer' src={angela} alt="Angela's image" />
           <div className='ml-3 text-[0.9rem]'>
             <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer transition ease-in duration-150'>Angela Gray</span> followed you<GoDotFill className={`inline ${clear || show[1]?'text-White':'text-Red'}  h-4 w-4`}/></p>
-            <p className='text-Lightgrayishblue2'>5m ago</p>
+            <p className='text-Grayishblue'>5m ago</p>
           </div>
         </div>
 
@@ -54,7 +54,7 @@ function App() {
           <img className='h-[2.8rem] cursor-pointer' src={jacob} alt="Jacob's image" />
           <div className='ml-3 text-[0.9rem]'>
             <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer transition ease-in duration-150'>Jacob Thompson</span> has joined your group <span className='font-semibold text-Blue cursor-pointer transition ease-in duration-150'>Chess Club</span><GoDotFill className={`inline ${clear || show[2]?'text-White':'text-Red'}  h-4 w-4`}/></p>
-            <p className='text-Lightgrayishblue2'>1 day ago</p>
+            <p className='text-Grayishblue'>1 day ago</p>
           </div>
         </div>
 
@@ -62,7 +62,7 @@ function App() {
           <img className='h-[2.8rem] cursor-pointer' src={rizky} alt="Rizky's image" />
           <div className='ml-3 text-[0.9rem]'>
             <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer transition ease-in duration-150'>Rizky Hasanuddin</span> sent you a private message</p>
-            <p className='text-Lightgrayishblue2'>5 days ago</p>
+            <p className='text-Grayishblue'>5 days ago</p>
             <p className='text-Grayishblue border p-4 rounded-md mt-4 outline-none border-Lightgrayishblue2 hover:border-Verylightgrayishblue cursor-pointer hover:bg-Verylightgrayishblue transition ease-in duration-150'>Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.</p>
           </div>
         </div>
@@ -72,7 +72,7 @@ function App() {
             <img className='h-[2.8rem] cursor-pointer' src={kimberly} alt="Kimberly's image" />
             <div className='ml-3 text-[0.9rem]'>
               <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer transition ease-in duration-150'>Kimberly Smith</span> commented on your picture</p>
-              <p className='text-Lightgrayishblue2'>1 week ago</p>
+              <p className='text-Grayishblue'>1 week ago</p>
             </div>
           </div>
           <img className='h-[2.8rem] ml-2 cursor-pointer' src={chessImg} alt="chess image" />
@@ -82,7 +82,7 @@ function App() {
           <img className='h-[2.8rem] cursor-pointer' src={nathan} alt="Nathan's image" />
           <div className='ml-3 text-[0.9rem]'>
             <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer transition ease-in duration-150'>Nathan Peterson</span> reacted to your recent post <span className='font-semibold text-Darkgrayishblue hover:text-Blue cursor-pointer transition ease-in duration-150'>5 end-game strategies to increase your win rate</span></p>
-            <p className='text-Lightgrayishblue2'>2 weeks ago</p>
+            <p className='text-Grayishblue'>2 weeks ago</p>
           </div>
         </div>
 
@@ -90,7 +90,7 @@ function App() {
           <img className='h-[2.8rem] cursor-pointer' src={anna} alt="Anna's image" />
           <div className='ml-3 text-[0.9rem]'>
             <p className='text-Grayishblue'><span className='font-semibold text-Verydarkblue hover:text-Blue cursor-pointer transition ease-in duration-150'>Anna Kim</span> left the group <span className='font-semibold text-Blue cursor-pointer transition ease-in duration-150'>Chess Club</span></p>
-            <p className='text-Lightgrayishblue2'>2 weeks ago</p>
+            <p className='text-Grayishblue'>2 weeks ago</p>
           </div>
         </div>
       </div>
